2ca334f9c1cb8f1a2a21d6f02250f51733f111fb
amador_jose <amadorjose@gmail.com>
2022-08-27 17:36:21 -0300

    Lista la fecha. Se puede afinar un toque más el formato pero funca.

--- homepage/models.py
+++ homepage/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from datetime import datetime, date 
 # Create your models here.
 
 class Familiar(models.Model):
   nombre = models.CharField(max_length=250)
   documento = models.CharField(max_length=250)
-  nacimiento = models.CharField(max_length=250)
+  nacimiento = models.DateField(auto_now_add=False, auto_now=False, blank=True)
 
   def __str__(self):
       return self.nombre
