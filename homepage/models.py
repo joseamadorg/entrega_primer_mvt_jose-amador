bca83b026fe459fb6138117d52222f93c5cb358c
amador_jose <amadorjose@gmail.com>
2022-08-27 16:16:59 -0300

    MVT funcionando. Falta arreglar la fecha. (Leyendo la documentación)

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Familiar(models.Model):
+  nombre = models.CharField(max_length=250)
+  documento = models.CharField(max_length=250)
+  nacimiento = models.CharField(max_length=250)
+
+  def __str__(self):
+      return self.nombre
+
+  def __num__(self):
+      return self.documento
+
+  def __date__(self):
+      return self.nacimiento
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Familiar
+
+
+def index(request):
+  familiars_list = Familiar.objects.all()
+  context = {'familiars_list': familiars_list}
+  return render(request, 'homepage/index.html', context=context)
